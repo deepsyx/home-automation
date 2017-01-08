19b5efa01d8a00c8679440bce793bef601b39c4f
Viktor <deepsybg@gmail.com>
2017-01-08 02:37:35 +0200

    add ac support and led strip

--- modules/ac.js
+++ modules/ac.js
@@ -1,40 +1,21 @@
-const rpio = require('rpio');
+const exec = require('child_process').execSync;
 
-const RELAY_PIN = 7;
-const SERVO_PIN = 32;
+let mode = 'off';
 
-rpio.open(RELAY_PIN, rpio.OUTPUT, rpio.HIGH);
-
-rpio.open(SERVO_PIN, rpio.PWM);
-rpio.pwmSetClockDivider(256);
-rpio.pwmSetRange(SERVO_PIN, 1500);
-
-let pwmPercentage = 0;
-
-function calculatePwm (percentage) {
-	return percentage / 100 * 145 + 40;
-}
-
-rpio.pwmSetData(SERVO_PIN, calculatePwm(0));
+const IRSEND_CMD = 'irsend SEND_ONCE MY_REMOTE ';
+exec(IRSEND_CMD + mode);
 
 module.exports = function (key, value, broadcast) {
-	if (key === 'HEATING') {
-		const status = Number(value);
-		rpio.write(RELAY_PIN, status ? rpio.HIGH : rpio.LOW);
+	if (key === 'AC') {
+		// mode, fan, degree
+		// irsend SEND_ONCE MY_REMOTE cool_swingauto_fanauto_30
+		const pieces = value.toLowerCase().split(':');
+		const mode = pieces[0] + '_swingauto_fan' + pieces[1] + '_' + degree;
+		exec(IRSEND_CMD + mode);
+		mode = value;
 	}
 
-	if (key === 'HEATING' || key === 'STATUS') {
-		broadcast('HEATING', rpio.read(RELAY_PIN));
-	}
-
-
-	if (key === 'HEATING_VALUE') {
-		const percentage = Number(value);
-		pwmPercentage = percentage;
-		rpio.pwmSetData(SERVO_PIN, calculatePwm(percentage));
-	}
-
-	if (key === 'HEATING_VALUE' || key === 'STATUS') {
-		broadcast('HEATING_VALUE', pwmPercentage);
+	if (key === 'STATUS' || key === 'AC') {
+		broadcast('AC', mode);
 	}
 }
---
+++ modules/lighting.js
@@ -0,0 +1,41 @@
+const rpio = require('rpio');
+
+const FLOOR_LED_STRIP_PINS = [11, 15, 13]; // 17, 22, 27
+const SOFA_LED_STRIP_PIN = 33;
+
+FLOOR_LED_STRIP_PINS.forEach((pinId) => rpio.open(pinId, rpio.OUTPUT, rpio.LOW));
+rpio.open(SOFA_LED_STRIP_PIN, rpio.LOW);
+
+module.exports = function (key, value, broadcast) {
+	if (key === 'LED_FLOOR') {
+		const pieces = value.split(':');
+
+		if (pieces.length !== 3) {
+			return console.warn('invalid LED_FLOOR command: ' + value);
+		}
+
+		[0, 1, 2].forEach((id) => {
+			rpio.write(
+				FLOOR_LED_STRIP_PINS[id],
+				pieces[id] === 1 ? rpio.HIGH : rpio.LOW
+			);
+		});
+	}
+
+	if (key === 'LED_FLOOR' || key === 'STATUS') {
+		const status = [0, 1, 2].map((id) => {
+			return rpio.read(FLOOR_LED_STRIP_PINS[id]);
+		}).join(':');
+
+		broadcast('LED_FLOOR', status);
+	}
+
+	if (key === 'LED_SOFA') {
+		const value = parseInt(value);
+		rpio.write(SOFA_LED_STRIP_PIN, value === 100 ? rpio.HIGH : rpio.LOW);
+	}
+
+	if (key === 'LED_SOFA' || key === 'STATUS') {
+		broadcast('LED_SOFA', rpio.read(SOFA_LED_STRIP_PIN))
+	}
+}
