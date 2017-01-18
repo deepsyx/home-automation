85ce3630a108fc2b6a7be0b2d44b8db74fa76d99
Viktor <deepsybg@gmail.com>
2017-01-18 22:37:37 +0200

    convert es6 to es5 export

--- utils/pi-blaster.js
+++ utils/pi-blaster.js
@@ -1,5 +1,5 @@
 const exec = require('child_process').execSync;
 
-export default function (pin, value) {
+module.exports = function (pin, value) {
 	return exec(`echo "${pin}=${parseFloat(value)}" > /dev/pi-blaster`);
 };
