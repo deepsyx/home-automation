7ca1f0f0e764cc5d2c2355fdee55e146a837f75e
Viktor <deepsybg@gmail.com>
2017-01-25 00:11:39 +0200

    Rename index container to app

--- phone-app/index.android.js
+++ phone-app/index.android.js
@@ -2,6 +2,6 @@ import {
   AppRegistry,
 } from 'react-native';
 
-import App from './app/containers';
+import App from './app/containers/App';
 
 AppRegistry.registerComponent('HomeApp', () => App);
